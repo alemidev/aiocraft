951c5bb147016873a2659242079efeef1fb14634
alemi <me@alemi.dev>
2022-01-06 15:46:06 +0100

    added enchantments to definitions

--- aiocraft/mc/definitions.py
+++ aiocraft/mc/definitions.py
@@ -56,6 +56,38 @@ class Gamemode(Enum):
 	ADVENTURE = 2
 	SPECTATOR = 3
 
+class Enchantments(Enum):
+	PROTECTION            = 0
+	FIRE_PROTECTION       = 1
+	FEATHER_FALLING       = 2
+	BLAST_PROTECTION      = 3
+	PROJECTILE_PROTECTION = 4
+	RESPIRATION           = 5
+	AQUA_AFFINITY         = 6
+	THORNS                = 7
+	DEPTH_STRIDER         = 8
+	FROST_WALKER          = 9
+	BINDING_CURSE         = 10
+	SHARPNESS             = 16
+	SMITE                 = 17
+	BANE_OF_ARTHROPODS    = 18
+	KNOCKBACK             = 19
+	FIRE_ASPECT           = 20
+	LOOTING               = 21
+	SWEEPING              = 22
+	EFFICIENCY            = 32
+	SILK_TOUCH            = 33
+	UNBREAKING            = 34
+	FORTUNE               = 35
+	POWER                 = 48
+	PUNCH                 = 49
+	FLAME                 = 50
+	INFINITY              = 51
+	LUCK_OF_THE_SEA       = 61
+	LURE                  = 62
+	MENDING               = 70
+	VANISHING_CURSE       = 71
+
 class ConnectionState(Enum):
 	NONE = -1
 	HANDSHAKING = 0
