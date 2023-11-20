f5ae50bfd7b8e9ae0926da16e45a8bca37e0a6cd
alemi <me@alemi.dev>
2023-11-20 23:45:09 +0100

    chore: moved all source under src

--- src/aiocraft/__init__.py
+++ src/aiocraft/__init__.py
@@ -5,7 +5,7 @@ from .server import AbstractMinecraftServer
 from .types import *
 from .auth import MicrosoftAuthenticator, MojangAuthenticator
 
-from .aiocraft import * # This is needed for PyO3 functions! No clue why or how...
+from .aiocraft import * # TODO why does PyO3 use the Cargo package name as top level name too??
 
 __author__ = "alemidev"
 __credits__ = "Thanks to pyCraft, really inspired this"
